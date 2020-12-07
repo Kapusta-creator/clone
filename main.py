f14495267b370d8de6a66ccb40dd72fa0864a084
Kapusta-creator <andreyiv132@yandex.ru>
2020-12-07 15:11:48 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def my_superfunction():
+    print("what an awesome print!")
+
+
+def main():
+    print("My first git program")
+    print("And I change it every day")
+    print("again")
+
+
+if __name__ == "__main__":
+    main()
